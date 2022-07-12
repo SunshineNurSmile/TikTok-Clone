7bb96a953fa90e2ea2a291a9d9f5001138dc092a
Yuanyi Wang <yuanyiw@bu.edu>
2022-07-12 05:00:36 -0400

    TikTok Clone

---
+++ components/SuggestedAccounts.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SuggestedAccounts = () => {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+};
+
+export default SuggestedAccounts;
